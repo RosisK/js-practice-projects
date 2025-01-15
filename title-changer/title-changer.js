96abfd91f086d004b20e30d3612c8b43d94804fa
RosisK <rosiskharel5@gmail.com>
2025-01-15 14:50:38 +0545

    Minor commit

--- title-changer/title-changer.js
+++ title-changer/title-changer.js
@@ -5,11 +5,14 @@ const titleObj = document.querySelector("title");
 // Changes the title
 const changeTitle = () => {
     titleObj.innerHTML = inputBox.value;
+    inputBox.value = ""; // Clearing the box at the end
 }
 
+// Event listeners
 changeBtn.addEventListener("click", changeTitle);
 inputBox.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         changeTitle();
     }
 });
+
